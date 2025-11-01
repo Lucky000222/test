9e28968505507fb1e25abee976ec781b74d59ce2
lucky000222 <79687258+Lucky000222@users.noreply.github.com>
2025-11-01 17:40:00 +0800

    Add files via upload

--- src/app/api/validate-wallet/route.js
+++ src/app/api/validate-wallet/route.js
@@ -15,9 +15,31 @@ export async function GET(request) {
   }
 
   try {
-    const queryResponse = await fetch("https://four.meme/mapi/defi/v2/public/wallet-direct/wallet/address/verify?address=" + address + "&projectId=meme_100567380&timestamp=" + timestamp,
-
-    )
+    const queryResponse = await fetch("https://four.meme/mapi/defi/v2/public/wallet-direct/wallet/address/verify?address=" + address + "&projectId=meme_100567380&timestamp=" + timestamp, {
+      "headers": {
+        "accept": "application/json, text/plain, */*",
+        "accept-language": "zh-CN,zh;q=0.9",
+        "if-none-match": "W/\"018ed0438666cf040556371eab2e58b11\"",
+        // "priority": "u=1, i",
+        "sec-ch-ua": "\"Google Chrome\";v=\"141\", \"Not?A_Brand\";v=\"8\", \"Chromium\";v=\"141\"",
+        "sec-ch-ua-arch": "\"x86\"",
+        "sec-ch-ua-bitness": "\"64\"",
+        "sec-ch-ua-full-version": "\"141.0.7390.123\"",
+        "sec-ch-ua-full-version-list": "\"Google Chrome\";v=\"141.0.7390.123\", \"Not?A_Brand\";v=\"8.0.0.0\", \"Chromium\";v=\"141.0.7390.123\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-model": "\"\"",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-ch-ua-platform-version": "\"19.0.0\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin"
+      },
+      "referrer": "https://four.meme/token/0x59537849f2a119ec698c7aa6c6daadc40c398a25",
+      "body": null,
+      "method": "GET",
+      "mode": "cors",
+      "credentials": "omit"
+    });
     // console.log(await queryResponse.text(), "queryResponse.text()", await queryResponse.status);
 
     const queryData = await queryResponse.text();
